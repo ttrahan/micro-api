36b7556981812cb91c838e89692c9619b0ba03f3
ttrahan <tom.trahan@gmail.com>
2016-02-28 17:04:38 -0800

    add gcloud cli install to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,10 @@ mv /root/micro-image/node_modules .
 
 # install node modules. the above mv will make this zip through as nothing new is installed
 npm install
+
+# install gcloud cli
+echo "================= Adding gclould binaries ============ "
+CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
