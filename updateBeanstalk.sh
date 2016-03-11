0ef06e97ff88056e460d884b73bc86e373331a16
Tom Trahan <tom@shippable.com>
2016-03-11 14:32:42 -0800

    update EB script

--- updateBeanstalk.sh
+++ updateBeanstalk.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [$BRANCH = "master"]; then
+if [ "$BRANCH" = "master" ]; then
 
   # create new Elastic Beanstalk application version
   aws elasticbeanstalk create-application-version --application-name $AWS_EB_APPLICATION --version-label $ACCOUNT_IDENTIFIER.$AWS_EB_ENVIRONMENT.$BRANCH.$BUILD_NUMBER --source-bundle S3Bucket=$AWS_S3_BUCKET,S3Key=$AWS_S3_FOLDER/$AWS_DEPLOY_JSON.$BRANCH.$BUILD_NUMBER --region=$AWS_REGION
@@ -9,5 +9,6 @@ if [$BRANCH = "master"]; then
 
   # deploy new application version to Elastic Beanstalk
   aws elasticbeanstalk update-environment --environment-name $AWS_EB_ENVIRONMENT --version-label $ACCOUNT_IDENTIFIER.$AWS_EB_ENVIRONMENT.$BRANCH.$BUILD_NUMBER --region=$AWS_REGION
-
+else
+  echo "Not on master branch - will not update Elastic Beanstalk environment"
 fi
