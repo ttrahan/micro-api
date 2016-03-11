55de2efc75e9da26806140cef1d1bc8ab9c25466
Tom Trahan <tom@shippable.com>
2016-03-11 11:32:35 -0800

    move eb updates to script

---
+++ updateBeanstalk.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [$BRANCH = "master"]; then
+
+  # create new Elastic Beanstalk application version
+  aws elasticbeanstalk create-application-version --application-name $AWS_EB_APPLICATION --version-label $ACCOUNT_IDENTIFIER.$AWS_EB_ENVIRONMENT.$BRANCH.$BUILD_NUMBER --source-bundle S3Bucket=$AWS_S3_BUCKET,S3Key=$AWS_S3_FOLDER/$AWS_DEPLOY_JSON.$BRANCH.$BUILD_NUMBER --region=$AWS_REGION
+
+  sleep 20s #This sleep is to make sure the image is available in the registry
+
+  # deploy new application version to Elastic Beanstalk
+  aws elasticbeanstalk update-environment --environment-name $AWS_EB_ENVIRONMENT --version-label $ACCOUNT_IDENTIFIER.$AWS_EB_ENVIRONMENT.$BRANCH.$BUILD_NUMBER --region=$AWS_REGION
+
+fi
