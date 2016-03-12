4e043718dfceaeb3cf56c2f03dba69285e629e66
Tom Trahan <tom@shippable.com>
2016-03-11 16:35:42 -0800

    update EB script

--- updateBeanstalk.sh
+++ updateBeanstalk.sh
@@ -9,9 +9,7 @@ if [ "$BRANCH" = "amazon-eb-cli-beta" ]; then
 
   # update the Elastic Beanstalk Dockerrun.aws.json deployment spec and copy files to S3
   cat Dockerrun.aws.json | sed "s/<IMAGE_NAME>/$REGISTRY_ACCOUNT\/$AWS_EB_APPLICATION\/$AWS_EB_ENVIRONMENT/" | sed "s/<TAG>/$BRANCH.$BUILD_NUMBER/" | sed "s/<DOCKER_CONFIG>/$AWS_EB_APPLICATION\/$AWS_EB_ENVIRONMENT\/$AWS_DOCKER_CONFIG.$BRANCH.$BUILD_NUMBER/" > $AWS_DEPLOY_JSON.$BRANCH.$BUILD_NUMBER
-
   aws s3 cp $AWS_DEPLOY_JSON.$BRANCH.$BUILD_NUMBER s3://$AWS_S3_BUCKET/$AWS_S3_FOLDER/$AWS_DEPLOY_JSON.$BRANCH.$BUILD_NUMBER --region=$AWS_REGION
-
   aws s3 cp /root/.docker/config.json s3://$AWS_S3_BUCKET/$AWS_S3_FOLDER/$AWS_DOCKER_CONFIG.$BRANCH.$BUILD_NUMBER --region=$AWS_REGION
 
   # create new Elastic Beanstalk application version
