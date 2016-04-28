0397060091e270e19aa38fa26f4b4daddcdde8b3
Tom Trahan <tom@shippable.com>
2016-04-28 08:48:00 -0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ cd /root/micro-api
 
 # get the pre installed nodemodules to here to save demo time
 # comment this out to do native install
-mv /root/micro-image/node_modules .
+# mv /root/micro-image/node_modules .
 
 # install node modules. the above mv will make this zip through as nothing new is installed
-npm install
+npm install --production
 
-# install awscli
-apt-get remove python-pip
-easy_install pip
-SUDO=$(which sudo) && $SUDO pip install awscli
+# # install awscli
+# apt-get remove python-pip
+# easy_install pip
+# SUDO=$(which sudo) && $SUDO pip install awscli
