87362c5eaa9cf8e958a0b3ef6fc89cc0da35dc8c
Tom Trahan <tom@shippable.com>
2016-04-29 16:28:08 -0700

    shorten rc image names

--- build_tag_release.sh
+++ build_tag_release.sh
@@ -2,16 +2,16 @@
 
 # build beta images for GitHub tags
 if [[ "$GIT_TAG_NAME" == *beta* ]]; then
-  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME .
+  docker build -t $REGISTRY_ACCOUNT/$SERVICE/beta:$GIT_TAG_NAME .
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME
 
 elif [[ "$GIT_TAG_NAME" == *test* ]]; then
-  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME .
+  docker build -t $REGISTRY_ACCOUNT/$SERVICE/test:$GIT_TAG_NAME .
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME
 
 elif [[ "$GIT_TAG_NAME" == *prod* ]]; then
-  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME
+  docker build -t $REGISTRY_ACCOUNT/$SERVICE/prod:$GIT_TAG_NAME .
+  docker push $REGISTRY_ACCOUNT/$SERVICE/prod:$GIT_TAG_NAME
 fi
 
 # build images for GitHub releases
