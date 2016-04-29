03b039e7155981c6e8afa6e66243c9ec83db7b55
Tom Trahan <tom@shippable.com>
2016-04-29 14:32:39 -0700

    add image tags for releases [ci skip]

--- build_tag_release.sh
+++ build_tag_release.sh
@@ -3,15 +3,24 @@
 # build images when tagged or marked as releases
 
 if [[ "$GIT_TAG_NAME" == *beta* ]]; then
-  echo "beta build processing..."
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME .
+  if [["$IS_RELEASE" == true ]]; then
+    docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME
+    docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME
+  fi
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME
 elif [[ "$GIT_TAG_NAME" == *test* ]]; then
-  echo "test build processing..."
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME .
+  if [["$IS_RELEASE" == true ]]; then
+    docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
+    docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
+  fi
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME
 elif [[ "$GIT_TAG_NAME" == *prod* ]]; then
-  echo "prod build processing..."
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME .
+  if [["$IS_RELEASE" == true ]]; then
+    docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
+    docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
+  fi
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME
 fi
