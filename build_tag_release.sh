7b2c8670175cd91ea0889757105b5974a591f588
Tom Trahan <tom@shippable.com>
2016-04-29 15:08:37 -0700

    update release builds

--- build_tag_release.sh
+++ build_tag_release.sh
@@ -2,28 +2,20 @@
 
 # build beta images for GitHub tags
 if [[ "$GIT_TAG_NAME" == *beta* ]]; then
-  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME
+  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME.beta .
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME.beta
 
 elif [[ "$GIT_TAG_NAME" == *test* ]]; then
-  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME
+  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME.test .
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME.test
 
 elif [[ "$GIT_TAG_NAME" == *prod* ]]; then
-  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME
+  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME.prod .
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/:$GIT_TAG_NAME.prod
 fi
 
 # build images for GitHub releases
 if [["$IS_RELEASE" == true ]]; then
-  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME
-
-elif [["$IS_RELEASE" == true ]]; then
-  docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
-
-elif [["$IS_RELEASE" == true ]]; then
-  docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
+  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/$RELEASE_NAME .
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/$RELEASE_NAME
 fi
