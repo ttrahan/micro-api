023c4d48da4bb5b07e07267878548de8abdfa586
Tom Trahan <tom@shippable.com>
2016-04-29 09:56:12 -0700

    build for tags-releases

--- build_tag_release.sh
+++ build_tag_release.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -x
 
-# push beta images
+# build images when tagged or marked as releases
 
 if [[ "$GIT_TAG_NAME" == *beta* ]]; then
+  echo "beta build processing..."
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME
 elif [[ "$GIT_TAG_NAME" == *test* ]]; then
+  echo "test build processing..."
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME
 elif [[ "$GIT_TAG_NAME" == *prod* ]]; then
+  echo "prod build processing..."
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME .
-  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$GIT_TAG_NAME
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME
 fi
