16d2bee88fba34a4d38f9070f42fb9fbb5aefbb8
Tom Trahan <tom@shippable.com>
2016-04-29 14:58:24 -0700

    update script for building releases

--- build_tag_release.sh
+++ build_tag_release.sh
@@ -1,31 +1,29 @@
 #!/bin/bash -x
 
-# build images when tagged or marked as releases
-
-# build beta images for GitHub tags and releases
+# build beta images for GitHub tags
 if [[ "$GIT_TAG_NAME" == *beta* ]]; then
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME .
-  if [["$IS_RELEASE" == true ]]; then
-    docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME
-    docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME
-  fi
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$GIT_TAG_NAME
 
-# build test images for GitHub tags and releases
 elif [[ "$GIT_TAG_NAME" == *test* ]]; then
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME .
-  if [["$IS_RELEASE" == true ]]; then
-    docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
-    docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
-  fi
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$GIT_TAG_NAME
 
-# build prod images for GitHub tags and releases
 elif [[ "$GIT_TAG_NAME" == *prod* ]]; then
   docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME .
-  if [["$IS_RELEASE" == true ]]; then
-    docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
-    docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
-  fi
   docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$GIT_TAG_NAME
+fi
+
+# build images for GitHub releases
+if [["$IS_RELEASE" == true ]]; then
+  docker build -t $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME .
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/beta:$RELEASE_NAME
+
+elif [["$IS_RELEASE" == true ]]; then
+  docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/test:$RELEASE_NAME
+
+elif [["$IS_RELEASE" == true ]]; then
+  docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
+  docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE/prod:$RELEASE_NAME
 fi
