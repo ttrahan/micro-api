94445e9514889ad730d23fc581c987e7ecb36a2c
Tom Trahan <tom@shippable.com>
2016-05-24 11:02:01 -0700

    add push to Docker Hub

--- push_commit.sh
+++ push_commit.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -x
 
-# build images for commit
+# build and push images for commit
 docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:latest \
 $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$BRANCH.$BUILD_NUMBER
 
 docker push $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:$BRANCH.$BUILD_NUMBER
+
+# build additional image for Docker Hub
+
+docker tag $REGISTRY_ACCOUNT/$APPLICATION/$SERVICE:latest \
+ttrahan/$SERVICE:$BRANCH.$BUILD_NUMBER
+
+docker push ttrahan/$SERVICE:$BRANCH.$BUILD_NUMBER
